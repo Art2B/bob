ff06359874729862985929226d8d1ed1d5504062
Art2B <arthur.battut@gmail.com>
2018-05-30 22:26:33 +0200

    Add tests for helpers

--- bob/helpers.py
+++ bob/helpers.py
@@ -1,21 +1,23 @@
-def month_string_to_number(string):
+def month_string_to_number(month):
     m = {
         'jan': 1,
         'feb': 2,
         'mar': 3,
-        'apr':4,
-        'may':5,
-        'jun':6,
-        'jul':7,
-        'aug':8,
-        'sep':9,
-        'oct':10,
-        'nov':11,
-        'dec':12
-        }
-    s = string.strip()[:3].lower()
+        'apr': 4,
+        'may': 5,
+        'jun': 6,
+        'jul': 7,
+        'aug': 8,
+        'sep': 9,
+        'oct': 10,
+        'nov': 11,
+        'dec': 12
+    }
 
+    if isinstance(month, str) == False:
+        raise TypeError('Not a string')
     try:
+        s = month.strip()[:3].lower()
         out = m[s]
         return out
     except:
