e1c0053c1d7664045b9550dda1728d9dd7c38158
Arthur Battut <arthur.battut@ekino.com>
2018-06-08 17:18:17 +0200

    Add function to update twitter account name

--- bob/twitter.py
+++ bob/twitter.py
@@ -15,7 +15,7 @@ api = TwitterAPI(
 def tweet(text):
     # Tweet the event if localMode is false in config
     if config['main']['verbose'] == True:
-        print('Tweet: ' + text)
+        print('Twitter.py - Tweet: ' + text)
 
     if config['main']['localMode'] == False:
         r = api.request(
@@ -25,3 +25,30 @@ def tweet(text):
             }
         )
         return r.json()['id_str']
+
+def udpate_name(iteration_number):
+    new_name = config['twitter']['baseName'] + ' #' + str(iteration_number)
+
+    if config['main']['verbose'] == True:
+        print('Twitter.py - Update account name: ' + new_name)
+
+    if config['main']['localMode'] == False:
+        r = api.request(
+            'account/update_profile',
+            {
+                'name': new_name
+            }
+        )
+        return r.json()
+
+def update_profile_pic(base64Picture):
+    if config['main']['verbose'] == True:
+        print('Twitter.py - Update profile picture')
+
+    if config['main']['localMode'] == False:
+        r = api.request(
+            'account/update_profile_image',
+            {
+                'image': base64Picture
+            }
+        )
